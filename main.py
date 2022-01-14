5e25fb7c35c9d3d2faa291d7ef25a1791e17d5b7
JulianKarlBauer <juliankarlbauer@gmx.de>
2022-01-14 16:10:54 +0100

    Add reference implementation of algorithm markley2007 from christophhagen
    https://github.com/christophhagen/averaging-quaternions

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import numpy as np
 from scipy.spatial.transform import Rotation
+import mechinterfabric
 from mechinterfabric.visualization import plot_bunch_along_x
 import os
 import matplotlib.pyplot as plt
@@ -21,8 +22,8 @@ assert np.allclose(xyzw_to_wxyz(np.array([1, 2, 3, 4])), np.array([4, 1, 2, 3]))
 rot_1 = Rotation.from_rotvec(0 * np.array([1, 0, 0]))
 rot_2 = Rotation.from_rotvec(np.pi / 4 * np.array([1, 0, 0]))
 
-print(rot_1.as_matrix())
-print(rot_2.as_matrix())
+# print(rot_1.as_matrix())
+# print(rot_2.as_matrix())
 
 quat_1 = rot_1.as_quat()
 quat_2 = rot_2.as_quat()
@@ -32,13 +33,21 @@ bunch = [quat_1, quat_2]
 # bunch = list(map(xyzw_to_wxyz, bunch))
 bunch = np.vstack(bunch)
 
+
+quat_av = mechinterfabric.external.weightedAverageQuaternions(Q=bunch, w=[1, 0])
+
+rot_av = Rotation.from_quat(quat_av)
+mat_av = rot_av.as_matrix()
+print(mat_av)
+
+
 ###################################
 # Plot bunch of rotations
 
 fig = plt.figure()
 ax = fig.add_subplot(111, projection="3d")
 
-plot_bunch_along_x(ax=ax, bunch=[rot_1, rot_2, rot_1, rot_2])
+plot_bunch_along_x(ax=ax, bunch=[rot_1, rot_2, rot_av])
 
 path_picture = os.path.join(directory, "coords" + ".png")
 plt.savefig(path_picture)
--- mechinterfabric/__init__.py
+++ mechinterfabric/__init__.py
@@ -1,1 +1,2 @@
 from . import visualization
+from . import averageQuaternions as external
