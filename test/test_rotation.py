f14c53474ed6b8f63d3040158ff98077a1cd7e0f
JulianKarlBauer <juliankarlbauer@gmx.de>
2022-01-16 13:36:50 +0100

    Test rotation averaging method against scipy.spatial.Rotation.mean

--- test/test_rotation.py
+++ test/test_rotation.py
@@ -15,12 +15,37 @@ def test_average_quaternion():
 
     weights = np.array([1, 1]) / 2
 
-    quat_av_ref = averageQuaternions.weightedAverageQuaternions(
-        Q=bunch, w=weights
-    )
+    quat_av_ref = averageQuaternions.weightedAverageQuaternions(Q=bunch, w=weights)
 
     quat_av = mechinterfabric.rotation.average_quaternion(
         quaternions=bunch, weights=weights
     )
 
     assert np.allclose(quat_av_ref, quat_av)
+
+
+def test_average_quaternions_scipy_rotation_mean():
+    rotations = Rotation.from_rotvec(
+        np.array([0 * np.random.rand(3), np.pi / 4 * np.random.rand(3)])
+    )
+
+    weights = np.ones((len(rotations))) / len(rotations)
+
+    bunch = np.vstack([rot.as_quat() for rot in rotations])
+
+    quat_av = mechinterfabric.rotation.average_quaternion(
+        quaternions=bunch, weights=weights
+    )
+
+    quat_av_ref = rotations.mean(weights=weights).as_quat()
+
+    print(quat_av)
+    print(quat_av_ref)
+
+    assert np.allclose(quat_av, quat_av_ref)
+
+    print("asd")
+
+
+if __name__ == "__main__":
+    test_average_quaternions_scipy_rotation_mean()
