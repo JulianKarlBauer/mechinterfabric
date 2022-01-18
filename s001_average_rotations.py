623455304fff87f6d50d862dc1215b89d0e0d73c
JulianKarlBauer <juliankarlbauer@gmx.de>
2022-01-18 19:12:09 +0100

    Plot granular interpolation between two orientations

--- s001_average_rotations.py
+++ s001_average_rotations.py
@@ -2,6 +2,7 @@ import numpy as np
 from scipy.spatial.transform import Rotation
 import mechinterfabric
 from mechinterfabric.visualization import plot_bunch_of_cos3D_along_x
+from mechinterfabric.visualization import plot_stepwise_interpolation_rotations_along_x
 import os
 import matplotlib.pyplot as plt
 
@@ -46,11 +47,17 @@ quat_pairs = {
             np.array([[0.0, 1.0, -0.0], [1.0, 0.0, -0.0], [0.0, 0.0, -1.0]])
         ).as_quat(),
     ),
+    "Exchange x and y axes without signs": (
+        Rotation.from_rotvec(0 * np.array([1, 0, 0])).as_quat(),
+        Rotation.from_matrix(
+            np.array([[0.0, 1.0, 0.0], [1.0, 0.0, 0.0], [0.0, 0.0, -1.0]])
+        ).as_quat(),
+    ),
 }
 
 for key, (quat_1, quat_2) in quat_pairs.items():
     quat_av = mechinterfabric.rotation.average_quaternion(
-        quaternions=np.vstack([quat_1, quat_2]), weights=np.ones(2) / 2
+        quaternions=np.vstack([quat_1, quat_2]), weights=[2 / 3, 1 / 3]
     )
 
     ###################################
@@ -59,12 +66,23 @@ for key, (quat_1, quat_2) in quat_pairs.items():
     fig = plt.figure()
     ax = fig.add_subplot(111, projection="3d")
 
-    plot_bunch_of_cos3D_along_x(
-        ax=ax,
-        bunch=list(
-            map(lambda x: Rotation.from_quat(x).as_matrix(), [quat_1, quat_av, quat_2])
-        ),
-    )
+    if False:
+        # Plot only end points and one intermediate
+        plot_bunch_of_cos3D_along_x(
+            ax=ax,
+            bunch=list(
+                map(
+                    lambda x: Rotation.from_quat(x).as_matrix(),
+                    [quat_1, quat_av, quat_2],
+                )
+            ),
+        )
+    elif True:
+        # Plot granularly
+        plot_stepwise_interpolation_rotations_along_x(
+            ax, quat_1, quat_2, nbr_points=7, scale=1, color="green"
+        )
+
     ax.set_title(key)
 
     path_picture = os.path.join(directory, "coords" + "_" + key + ".png")
