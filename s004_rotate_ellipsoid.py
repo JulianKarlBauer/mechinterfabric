b9e42cca432c1e0fcf01978b8c91d9fe6a5a3505
JulianKarlBauer <juliankarlbauer@gmx.de>
2022-01-18 16:21:36 +0100

    clean and restructure

--- s004_rotate_ellipsoid.py
+++ s004_rotate_ellipsoid.py
@@ -9,16 +9,22 @@ os.makedirs(directory, exist_ok=True)
 
 
 # N1 = np.diag([0.7, 0.2, 0.1])
-N1 = np.diag([0.5, 0.3, 0.2])
-direction = np.array([1, 0, 0])
-rot_vec = 1 / 8 * np.pi * direction / np.linalg.norm(direction)
-rotation = Rotation.from_rotvec(rot_vec)
-transform = rotation.as_matrix()
+N1 = np.diag([0.95, 0.05, 0.0])
 
+if False:
+    direction = np.array([1, 0, 0])
+    rot_vec = 1 / 2 * np.pi * direction / np.linalg.norm(direction)
+    rotation = Rotation.from_rotvec(rot_vec)
+
+elif True:
+    rotation = Rotation.from_matrix(
+        np.array([[0.0, 1.0, -0.0], [1.0, 0.0, -0.0], [0.0, 0.0, -1.0]])
+    )
+
+transform = rotation.as_matrix()
 N2 = np.einsum("ki, lj, ij->kl", transform, transform, N1)
 
 N1_eigenvals, N1_matrix = mechinterfabric.utils.get_rotation_matrix_into_eigensystem(N1)
-
 N2_eigenvals, N2_matrix = mechinterfabric.utils.get_rotation_matrix_into_eigensystem(N2)
 
 N1_rotation = Rotation.from_matrix(N1_matrix)
@@ -28,18 +34,6 @@ validation = Rotation.from_matrix(rotation.as_matrix())
 assert np.allclose(validation.as_matrix(), rotation.as_matrix())
 assert np.allclose(validation.as_rotvec(), rotation.as_rotvec())
 
-
-print("#######################")
-print(transform)
-print(N2_rotation.as_matrix())
-
-print(rotation.as_rotvec())
-print(N2_rotation.as_rotvec())
-
-func = mechinterfabric.utils.assert_ortho_normal_right_handed
-func(N1_rotation.as_matrix())
-func(N2_rotation.as_matrix())
-func(transform)
 
 # Plot
 
@@ -52,57 +46,60 @@ ax.set_ylabel("y")
 ax.set_zlabel("z")
 
 ###################
-ax.cos3D(
-    origin=[0, 1, 0],
-    length=1 / 2,
-    matrix=np.eye(3),
-)
+# Plot N1
 
 origin = [0, 0, 0]
-arrow_x = mechinterfabric.visualization.Arrow3D(
-    *[0, 0, 0], *rot_vec * 0.3, ec="black", fc="black"
-)
-ax.add_artist(arrow_x)
 
 ax.cos3D(
-    origin=[-0.5, 1, 0],
+    origin=origin,
     length=1 / 2,
-    matrix=rotation.as_matrix(),
+    matrix=N1_matrix,
 )
-
-ax.cos3D(
-    origin=[0, 1, 0],
-    length=1 / 2,
-    matrix=N2_matrix,
-)
-###################
-
 
 mechinterfabric.visualization.plot_ellipsoid(
     ax=ax,
-    origin=[1, 0, 0],
+    origin=origin,
     radii_in_eigen=N1_eigenvals,
     matrix_into_eigen=N1_matrix,
     color="red",
 )
+
+###################
+# Reference
+
+origin = [1, 0, 0]
+
+arrow_x = mechinterfabric.visualization.Arrow3D(
+    *origin, *rotation.as_rotvec() * 0.3, ec="black", fc="black"
+)
+ax.add_artist(arrow_x)
+
+ax.cos3D(
+    origin=origin,
+    length=1 / 2,
+    matrix=rotation.as_matrix(),
+)
+
+###################
+# N2
+
+origin = [2, 0, 0]
+
+ax.cos3D(
+    origin=origin,
+    length=1 / 2,
+    matrix=N2_matrix,
+)
+
 mechinterfabric.visualization.plot_ellipsoid(
     ax=ax,
-    origin=[1, 0, 0],
+    origin=origin,
     radii_in_eigen=N2_eigenvals,
     matrix_into_eigen=N2_matrix,
     color="green",
 )
 
-ax.cos3D(
-    origin=[0, 0, 0],
-    length=1 / 2,
-    matrix=N1_matrix,
-)
-ax.cos3D(
-    origin=[0, 0, 0],
-    length=1 / 2,
-    matrix=N2_matrix,
-)
+#########################################################
 
 bbox_min = -1
 bbox_max = 2
