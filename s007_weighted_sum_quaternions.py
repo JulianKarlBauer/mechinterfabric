476696d6e2fea636a761ce42ca092d73c8835072
JulianKarlBauer <juliankarlbauer@gmx.de>
2022-01-19 20:33:43 +0100

    Clean from elev azim quatsch

--- s007_weighted_sum_quaternions.py
+++ s007_weighted_sum_quaternions.py
@@ -31,41 +31,43 @@ for i in range(10):
         print(f"{'mean':<20}= {mean.as_rotvec()}")
         print(f"{'weighted sum quats':<20}= {weighted_sum.as_rotvec()}")
 
-        for elev in [10, 20]:
-            for azim in [0, 30, 60]:
+        # for elev in [10, 20]:
+        #     for azim in [0, 30, 60]:
+        elev = 10
+        azim = 30
 
-                fig = plt.figure()
-                ax = fig.add_subplot(111, projection="3d")
+        fig = plt.figure()
+        ax = fig.add_subplot(111, projection="3d")
 
-                ax.view_init(elev=elev, azim=azim)
+        ax.view_init(elev=elev, azim=azim)
 
-                mechinterfabric.visualization.plot_bunch_of_cos3D_along_x(
-                    ax=ax,
-                    bunch=list(
-                        map(
-                            lambda x: x.as_matrix(),
-                            [rotations[0], mean, rotations[1]],
-                        )
-                    ),
-                    shift_y=-0.2,
+        mechinterfabric.visualization.plot_bunch_of_cos3D_along_x(
+            ax=ax,
+            bunch=list(
+                map(
+                    lambda x: x.as_matrix(),
+                    [rotations[0], mean, rotations[1]],
                 )
+            ),
+            shift_y=-0.2,
+        )
 
-                mechinterfabric.visualization.plot_bunch_of_cos3D_along_x(
-                    ax=ax,
-                    bunch=list(
-                        map(
-                            lambda x: x.as_matrix(),
-                            [rotations[0], weighted_sum, rotations[1]],
-                        )
-                    ),
-                    shift_y=0.2,
+        mechinterfabric.visualization.plot_bunch_of_cos3D_along_x(
+            ax=ax,
+            bunch=list(
+                map(
+                    lambda x: x.as_matrix(),
+                    [rotations[0], weighted_sum, rotations[1]],
                 )
+            ),
+            shift_y=0.2,
+        )
 
-                title = f"{i:03}" + "_" + f"elev{elev:03}" + "_" + f"azim{azim:03}"
-                ax.set_title(title)
+        title = f"{i:03}" + "_" + f"elev{elev:03}" + "_" + f"azim{azim:03}"
+        ax.set_title(title)
 
-                path_picture = os.path.join(
-                    directory,
-                    "mean_vs_quaternionWeightedSum" + "_" + title + ".png",
-                )
-                plt.savefig(path_picture)
+        path_picture = os.path.join(
+            directory,
+            "mean_vs_quaternionWeightedSum" + "_" + title + ".png",
+        )
+        plt.savefig(path_picture)
