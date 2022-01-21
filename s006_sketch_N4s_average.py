f3305d21423b91f7276bc28ee905a50d6d92fd26
JulianKarlBauer <juliankarlbauer@gmx.de>
2022-01-21 12:47:44 +0100

    change name

--- s006_sketch_N4s_average.py
+++ s006_sketch_N4s_average.py
@@ -67,7 +67,9 @@ for key, (N4_1, N4_2) in pairs.items():
         N2_av_eigen,
         N4s_eigen,
         rotations,
-    ) = mechinterfabric.interpolation.interpolate_N4_decomp(N4s=N4s, weights=weights)
+    ) = mechinterfabric.interpolation.interpolate_N4_decomp_extended_return_values(
+        N4s=N4s, weights=weights
+    )
 
     ##########
     N4_av_mandel = con.to_mandel6(N4_av)
