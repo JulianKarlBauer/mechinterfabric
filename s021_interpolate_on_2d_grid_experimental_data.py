b2bc5ca915241f027778f5bf7e0ebbe8bacc57c0
JulianKarlBauer <juliankarlbauer@gmx.de>
2022-01-31 19:59:56 +0100

    read N4

--- s021_interpolate_on_2d_grid_experimental_data.py
+++ s021_interpolate_on_2d_grid_experimental_data.py
@@ -6,12 +6,15 @@ from mechinterfabric.utils import get_rotation_matrix_into_eigensystem
 import os
 import matplotlib.pyplot as plt
 import pandas as pd
+import itertools
 
 np.random.seed(seed=100)
 np.set_printoptions(linewidth=100000)
 
 directory = os.path.join("output", "s011")
 os.makedirs(directory, exist_ok=True)
+
+converter = mechkit.notation.Converter()
 
 #########################################################
 # N2
@@ -35,3 +38,52 @@ def N2_from_row(row):
 
 
 df_N2["N2"] = df_N2.apply(N2_from_row, axis=1)
+
+#########################################################
+# N4
+
+df = pd.read_csv(
+    os.path.join("data", "juliane_blarr_mail_2022_01_31_1124_N4.csv"),
+    header=0,
+    sep=",",
+)
+df.columns = df.columns.str.strip()
+
+
+def N4_from_row(row):
+    # N4 is completely index-symmetric
+    N4 = np.zeros((3, 3, 3, 3), dtype=np.float64)
+
+    columns = [
+        "3333",
+        "3332",
+        "3322",
+        "3222",
+        "2222",
+        "3331",
+        "3321",
+        "3221",
+        "2221",
+        "3311",
+        "3211",
+        "2211",
+        "3111",
+        "2111",
+        "1111",
+    ]
+
+    for column_key in columns:
+        permutations = itertools.permutations([int(item) - 1 for item in column_key])
+        for perm in permutations:
+            N4[perm] = row[column_key]
+    return N4
+
+    # for index_i in range(3):
+    #     for index_j in range(3):
+    #         for index_k in range(3):
+    #             for index_l in range(3):
+    #                 key = f"{index_i}{index_j}{index_k}{index_l}"
+    #
+    #                 N4[index_i, index_j, index_k, index_l] = 1
+
+df["N4"] = df.apply(N4_from_row, axis=1)
