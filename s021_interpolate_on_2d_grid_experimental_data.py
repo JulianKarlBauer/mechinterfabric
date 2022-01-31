1e41bc9afc6516c840252966d7d85f12315865c5
JulianKarlBauer <juliankarlbauer@gmx.de>
2022-01-31 20:00:59 +0100

    cledan for JB

--- s021_interpolate_on_2d_grid_experimental_data.py
+++ s021_interpolate_on_2d_grid_experimental_data.py
@@ -1,12 +1,8 @@
 import numpy as np
-from scipy.spatial.transform import Rotation
-import mechinterfabric
 import mechkit
-from mechinterfabric.utils import get_rotation_matrix_into_eigensystem
-import os
-import matplotlib.pyplot as plt
 import pandas as pd
 import itertools
+import os
 
 np.random.seed(seed=100)
 np.set_printoptions(linewidth=100000)
