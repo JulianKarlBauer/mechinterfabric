ac916b332e916725fea3c8791d8c22f72766ad8c
JulianKarlBauer <juliankarlbauer@gmx.de>
2022-02-02 10:36:12 +0100

    Refactor to utils: Get unique eigensystem of N4 is separate method

--- mechinterfabric/interpolation.py
+++ mechinterfabric/interpolation.py
@@ -128,69 +128,22 @@ def interpolate_N4_decomp(N4s, weights):
     return interpolate_N4_decomp_extended_return_values(N4s, weights)[0]
 
 
-def get_additional_rotation_into_unique_eigensystem(N4_tensor_in_eigen):
-    transforms_raw = utils.get_orthotropic_sym_rotations(as_dict=True)
-
-    index_d8 = np.s_[0, 0, 0, 1]
-    index_d6 = np.s_[0, 0, 0, 2]
-
-    d8 = N4_tensor_in_eigen[index_d8]
-    d6 = N4_tensor_in_eigen[index_d6]
-
-    if (0.0 <= d8) and (0.0 <= d6):
-        key = "no flip"
-    elif (0.0 <= d8) and (d6 < 0.0):
-        key = "flip xy"
-    elif (d8 < 0.0) and (0.0 <= d6):
-        key = "flip xz"
-    elif (d8 < 0.0) and (d6 < 0.0):
-        key = "flip yz"
-    else:
-        raise Exception("Unexpected")
-
-    return transforms_raw[key]
-
-
-def interpolate_N4_decomp_unique_rotation_extended_return_values(N4s, weights):
-    converter = mechkit.notation.ExplicitConverter()
-
-    def to_mandel6(N4s):
-
-        return converter.convert(
-            inp=N4s, source="tensor", target="mandel6", quantity="stiffness"
-        )
+def interpolate_N4_decomp_unique_rotation_extended_return_values(
+    N4s, weights, validate=True
+):
 
     utils.assert_notation_N4(N4s, weights)
 
-    I2 = mechkit.tensors.Basic().I2
-
-    N2s = np.einsum("mijkl,kl->mij", N4s, I2)
-
-    # Get rotations into eigensystem
-    eigenvals, rotations_non_unique = zip(
-        *[get_rotation_matrix_into_eigensystem(N2) for N2 in N2s]
-    )
-    rotations_non_unique = np.array(rotations_non_unique)
-
-    # Rotate each N4 into one possible eigensystem
-    N4s_eigen_non_unique = apply_rotation(rotations=rotations_non_unique, tensors=N4s)
-
-    # Get unique eigensystem
-    additional_rotation = np.array(
-        [
-            get_additional_rotation_into_unique_eigensystem(N4_tensor_in_eigen=N4_eigen)
-            for N4_eigen in N4s_eigen_non_unique
-        ]
-    )
-
-    rotations = np.einsum(
-        "...ij,...jk->...ik", rotations_non_unique, additional_rotation
-    )
+    (
+        rotations,
+        N2s,
+        eigenvals,
+        rotations,
+        additional_rotation,
+    ) = utils.get_rotation_matrix_into_unique_N4_eigensystem_detailed(N4s=N4s)
 
     # Rotate each N4 into the unique eigensystem
-    N4s_eigen = apply_rotation(
-        rotations=additional_rotation, tensors=N4s_eigen_non_unique
-    )
+    N4s_eigen = utils.apply_rotation(rotations=rotations, tensors=N4s)
 
     # Get average rotation
     rotation_av = Rotation.from_matrix(rotations).mean(weights=weights).as_matrix()
@@ -199,12 +152,15 @@ def interpolate_N4_decomp_unique_rotation_extended_return_values(N4s, weights):
     N4_av_eigen = np.einsum("m, mijkl->ijkl", weights, N4s_eigen)
 
     # Rotate back to world COS
-    N4_av = apply_rotation(rotations=rotation_av.T, tensors=N4_av_eigen)
+    N4_av = utils.apply_rotation(rotations=rotation_av.T, tensors=N4_av_eigen)
 
-    # Check if N4_av[I2] == N2_av
-    N4_av_I2_eigen = np.einsum("ijkl,kl->ij", N4_av_eigen, I2)
-    N2_av_eigen = np.diag(np.einsum("i, ij->j", weights, eigenvals))
-    assert np.allclose(N4_av_I2_eigen, N2_av_eigen)
+    if validate:
+        # Check if N4_av[I2] == N2_av
+        N4_av_I2_eigen = np.einsum(
+            "ijkl,kl->ij", N4_av_eigen, mechkit.tensors.Basic().I2
+        )
+        N2_av_eigen = np.diag(np.einsum("i, ij->j", weights, eigenvals))
+        assert np.allclose(N4_av_I2_eigen, N2_av_eigen)
 
     return N4_av, N4_av_eigen, rotation_av, N2_av_eigen, N4s_eigen, rotations
 
