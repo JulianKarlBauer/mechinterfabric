d8ba2a0acbe70d0968ca8d20b64a764caac9eb7e
JulianKarlBauer <juliankarlbauer@gmx.de>
2022-03-17 20:08:24 +0100

    Tweak x-axis transv.

--- s032_N4_fem_mesh_2D.py
+++ s032_N4_fem_mesh_2D.py
@@ -25,15 +25,15 @@ con = mechkit.notation.Converter()
 
 almost_zero = 1e-5
 
-index_low, index_medium, index_high = 1, 3, 5
+index_low, index_medium, index_high = 1, 4, 7
 
 df = pd.DataFrame(
     [
-        ["planar_iso", -2 / 6, 3 / 280, index_low, index_high],
-        ["iso", 0, 0, index_medium, index_medium],
+        # ["planar_iso", -2 / 6, 3 / 280, index_low, index_high],
+        ["iso", almost_zero, 0, index_medium, index_medium],
         ["ud", 4 / 6, 1 / 35, index_high, index_low],
-        ["iso2_max", 0, 1 / 60, index_high, index_high],
-        ["iso2_min", 0, -1 / 90, index_low, index_low],
+        ["iso2_max", almost_zero, 1 / 60, index_high, index_high],
+        ["iso2_min", almost_zero, -1 / 90, index_low, index_low],
     ],
     columns=["label", "alpha1", "rho1", "index_x", "index_y"],
 )
@@ -45,19 +45,19 @@ N4_func = sp.lambdify([alpha1, rho1], parameterization)
 
 df["N4"] = df.apply(lambda row: N4_func(alpha1=row["alpha1"], rho1=row["rho1"]), axis=1)
 
-Q = Rotation.from_rotvec(np.pi / 2 * np.array([0, 1, 0])).as_matrix()
-df = df.set_index("label")
-df.at["planar_iso", "N4"] = con.to_mandel6(
-    np.einsum(
-        "io,jm,kn,lp, omnp->ijkl",
-        Q,
-        Q,
-        Q,
-        Q,
-        con.to_tensor(df.loc["planar_iso"]["N4"]),
-    )
-)
-df.reset_index()
+# Q = Rotation.from_rotvec(np.pi / 2 * np.array([0, 1, 0])).as_matrix()
+# df = df.set_index("label")
+# df.at["planar_iso", "N4"] = con.to_mandel6(
+#     np.einsum(
+#         "io,jm,kn,lp, omnp->ijkl",
+#         Q,
+#         Q,
+#         Q,
+#         Q,
+#         con.to_tensor(df.loc["planar_iso"]["N4"]),
+#     )
+# )
+# df.reset_index()
 
 N4s = converter.convert(
     source="mandel6",
@@ -75,7 +75,11 @@ indices = [i + 1 for i in range(index_high)]
 # indices = [i + 1 for i in range(13) if i in [0, 3, 6, 9, 12]]
 
 indices_points = list(itertools.product(indices, repeat=2))
-indices_points = [index for index in indices_points if index not in df_index]
+indices_points = [
+    index
+    for index in indices_points
+    if ((index not in df_index) and (index[0] >= index[1]))
+]
 
 new = pd.DataFrame(indices_points, columns=["index_x", "index_y"])
 
@@ -188,9 +192,9 @@ for interpolation_method in [
         mlab.show()
 
 
-# Inspect unknown N4s
-tmp = new.set_index(["index_x", "index_y"])
-N4_new = con.to_mandel6(tmp.loc[(1, 2)]["N4"])
-print(np.linalg.eig(N4_new))
-print(np.einsum("i,ji->j", [1, 1, 1, 0, 0, 0], N4_new))
-print(sum(np.einsum("i,ji->j", [1, 1, 1, 0, 0, 0], N4_new)))
+# # Inspect unknown N4s
+# tmp = new.set_index(["index_x", "index_y"])
+# N4_new = con.to_mandel6(tmp.loc[(1, 2)]["N4"])
+# print(np.linalg.eig(N4_new))
+# print(np.einsum("i,ji->j", [1, 1, 1, 0, 0, 0], N4_new))
+# print(sum(np.einsum("i,ji->j", [1, 1, 1, 0, 0, 0], N4_new)))
