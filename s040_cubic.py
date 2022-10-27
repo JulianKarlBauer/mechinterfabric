1e76ff9f177bdea330dcaedda5071270edf96641
Julian Karl Bauer <juliankarlbauer@gmx.de>
2022-10-27 17:11:00 +0200

    Rotate: How to find eigensystem from N4-information?

--- s040_cubic.py
+++ s040_cubic.py
@@ -2,10 +2,17 @@ import vofotensors
 from pprint import pprint
 from vofotensors.abc import d1
 import sympy as sp
+import numpy as np
+import symbolic as sb
+from symbolic.numbers import z, one
+from symbolic.abc import alpha
 
 N4 = vofotensors.fabric_tensors.N4s_parametric['cubic']['d1']
 
 pprint(N4)
+
+#########################
+# Parameter limits
 
 # Mathematica
 
@@ -31,3 +38,25 @@ low = N4.subs({d1: sp.sympify(substitutions[0])})
 high = N4.subs({d1: sp.sympify(substitutions[1])})
 
 combined = w*low + (sp.S(1)-w) * high
+
+#########################
+# Rotate
+
+Q_around_z = np.array(
+    (
+        (sp.cos(alpha), -sp.sin(alpha), z),
+        (sp.sin(alpha), sp.cos(alpha), z),
+        (z, z, one),
+    ),
+)
+
+N4_rotated = sp.Matrix(sp.trigsimp(sb.actively_rotate_mandel(mandel=N4, Q=Q_around_z)))
+
+pprint(N4_rotated)
+
+I2 = sb.get_I2()
+
+N2 = sb.mandel(np.tensordot(sb.tensorr(N4), sb.to_numpy(sb.get_I2())))
+
+
+
