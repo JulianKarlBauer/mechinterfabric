029132ae4e216fe1df960e7ed453a6625efaced7
JulianKarlBauer <juliankarlbauer@gmx.de>
2023-01-11 22:24:17 +0100

    Sketch structure

--- mechinterfabric/__init__.py
+++ mechinterfabric/__init__.py
@@ -1,1 +1,14 @@
-print('here we go')
+import numpy as np
+
+
+class FourthOrderFabricAnalyser:
+    def __init__(self):
+        return None
+
+    def analyse(self, FOT4):
+        # Contract
+        # Get eigensystem candidates
+        # Identify symmetry
+        # Get parameters
+        result = {"d0": 0.0, "rotation_Q": np.eye(3)}
+        return result
---
+++ test/test_FourthOrderFabricAnalyser.py
@@ -0,0 +1,10 @@
+import mechinterfabric
+import numpy as np
+
+class TestAnalyser:
+    def test_init(self):
+        analyser = mechinterfabric.FourthOrderFabricAnalyser()
+
+    def test_analyse(self):
+        analyser = mechinterfabric.FourthOrderFabricAnalyser()
+        assert np.allclose(analyser.analyse(np.eye(6))["rotation_Q"], np.eye(3))
