a6b89373f93e90c8dd39d20a38511f4d9dafec5b
JulianKarlBauer <juliankarlbauer@gmx.de>
2023-01-11 21:50:49 +0100

    Add dummy package

---
+++ setup.py
@@ -0,0 +1,33 @@
+import setuptools
+from os import path
+import sys
+
+description = "Interpolation of fabric and fiber-orientation tensors"
+
+if sys.version_info > (3, 0):
+    this_directory = path.abspath(path.dirname(__file__))
+    with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
+        long_description = f.read()
+else:
+    long_description = description
+
+setuptools.setup(
+    name="mechinterfabric",
+    version="0.0.1",
+    author="Julian Karl Bauer",
+    author_email="JulianKarlBauer@gmx.de",
+    description=description,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/JulianKarlBauer/mechinterfabric",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        "setuptools",
+        "numpy",
+    ],
+    classifiers=[
+        "Programming Language :: Python",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
