c78952f41b45b3e686037efdd38a1024feac1a37
JulianKarlBauer <juliankarlbauer@gmx.de>
2023-02-04 01:13:13 +0100

    Prepare distinction FOT2-Sym

--- mechinterfabric/core.py
+++ mechinterfabric/core.py
@@ -10,6 +10,8 @@ class FOT4Analysis:
         self._assert_is_FOT4(FOT4)
         self.FOT4_mandel6 = converter.to_mandel6(FOT4)
 
+        self._keys_sym_FOT2 = ["isotropic", "transversely_isotropic", "orthotropic"]
+
     def calc_FOT2(self):
         self.FOT2 = self._contract_FOT4_to_FOT2(self.FOT4_mandel6)
         return self
@@ -27,21 +29,40 @@ class FOT4Analysis:
 
     def identify_symmetry_FOT2(self):
         index_pairs = [[0, 1], [0, 2], [1, 2]]
+
         self.FOT2_eigenvalues_are_equal = tuple(
             self._pair_of_eigenvalues_is_equal(*self.FOT2_eigenvalues[pair])
             for pair in index_pairs
         )
-        transv = "transversely_isotropic"
 
-        mapping = {
-            (True, True, True): "isotropic",
-            (True, True, False): transv,
-            # We assume, that eigenvalues are sorted, so v0 == v2 is only possible, if v0 == v1 as well
-            (False, True, True): transv,
-            (False, False, False): "orthotropic",
-        }
-        self.FOT2_symmetry = mapping[self.FOT2_eigenvalues_are_equal]
+        self.FOT2_symmetry = self._map_equal_eigenvalue_pairs_to_symmetry()
         return self
+
+    def _map_equal_eigenvalue_pairs_to_symmetry(self):
+        return {
+            (True, True, True): "isotropic",
+            (True, True, False): "transversely_isotropic",
+            # We assume, that eigenvalues are sorted, so v0 == v2 is only possible, if v0 == v1 as well
+            (False, True, True): "transversely_isotropic",
+            (False, False, False): "orthotropic",
+        }[self.FOT2_eigenvalues_are_equal]
+
+    def get_eigensystem(self):
+        func = getattr(
+            self,
+            {sym: f"_get_eigensystem_{sym}" for sym in self._keys_sym_FOT2}[
+                self.FOT2_symmetry
+            ],
+        )()
+
+    def _get_eigensystem_isotropic(self):
+        pass
+
+    def _get_eigensystem_transversely_isotropic(self):
+        pass
+
+    def _get_eigensystem_orthotropic(self):
+        pass
 
     def _pair_of_eigenvalues_is_equal(self, first, second, atol=1e-4, rtol=1e-4):
         return np.isclose(first, second, atol=atol, rtol=rtol)
@@ -78,11 +99,11 @@ class FourthOrderFabricAnalyser:
         print(analysis.FOT2_rotation)
         print(analysis.FOT4_mandel6_dev)
 
-        # Get eigensystem
-        # analysis.get_eigensystem()
-
         # Identify symmetry FOT2
         analysis.identify_symmetry_FOT2()
+
+        # Get eigensystem
+        analysis.get_eigensystem()
 
         analysis.result = {"d0": 0.0, "rotation_Q": np.eye(3)}
         return analysis
