06efc743edfdc1b1d02ffe3c7ffda00a128848de
JulianKarlBauer <juliankarlbauer@gmx.de>
2023-02-05 23:34:29 +0100

    Format

--- test/decompositions_test.py
+++ test/decompositions_test.py
@@ -25,6 +25,16 @@ def cubic_by_d1(lambdified_parametrization_cubic, request):
     return lambdified_parametrization_cubic(d1=request.param)
 
 
+f = np.array
+list_of_tests = [
+    (f([-1.3333e-01, -1.3333e-01, -1.3333e-01, 1.3010e-16, 2.0000e-01, 2.0000e-01]), 4),
+    (f([-7.7777e-02, -7.7777e-02, -7.7777e-02, 1.9602e-16, 1.1666e-01, 1.1666e-01]), 4),
+    (f([-2.2222e-02, -2.2222e-02, -2.2222e-02, 1.9689e-16, 3.3333e-02, 3.3333e-02]), 4),
+    (f([-5.0000e-02, -5.0000e-02, 1.2836e-16, 3.3333e-02, 3.3333e-02, 3.3333e-02]), 0),
+    (f([-1.3333e-01, -1.3333e-01, 1.6653e-16, 8.8888e-02, 8.8888e-02, 8.8888e-02]), 0),
+]
+
+
 @pytest.fixture()
 def func_rotating_FOT4(converter):
     def func(tensor_or_mandel):
@@ -49,18 +59,6 @@ def func_rotating_FOT4(converter):
     return func
 
 
-limits = (-1 / 15, 2 / 45)
-
-f = np.array
-list_of_tests = [
-    (f([-1.3333e-01, -1.3333e-01, -1.3333e-01, 1.3010e-16, 2.0000e-01, 2.0000e-01]), 4),
-    (f([-7.7777e-02, -7.7777e-02, -7.7777e-02, 1.9602e-16, 1.1666e-01, 1.1666e-01]), 4),
-    (f([-2.2222e-02, -2.2222e-02, -2.2222e-02, 1.9689e-16, 3.3333e-02, 3.3333e-02]), 4),
-    (f([-5.0000e-02, -5.0000e-02, 1.2836e-16, 3.3333e-02, 3.3333e-02, 3.3333e-02]), 0),
-    (f([-1.3333e-01, -1.3333e-01, 1.6653e-16, 8.8888e-02, 8.8888e-02, 8.8888e-02]), 0),
-]
-
-
 class TestSpectralDecomposititonOfCubicFOT4Deviator:
     @pytest.mark.parametrize("eigen_values,first_index", list_of_tests)
     def test__get_index_two_fold_eigenvalue_of_cubic_deviator(
@@ -80,7 +78,7 @@ class TestSpectralDecomposititonOfCubicFOT4Deviator:
 
     @pytest.mark.parametrize(
         "cubic_by_d1",
-        np.linspace(*limits, 3),
+        np.linspace(*(-1 / 15, 2 / 45), 3),
         indirect=True,
     )
     def test_eigenvectors_of_both_two_fold_eigen_values_lead_to_similar_rotation(
