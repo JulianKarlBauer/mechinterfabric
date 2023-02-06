1ed8f0b5d6af13e963d6f4adefd6430f71970379
JulianKarlBauer <juliankarlbauer@gmx.de>
2023-02-07 00:28:29 +0100

    Play

--- s005_eigenvectors_deviators.py
+++ s005_eigenvectors_deviators.py
@@ -5,13 +5,20 @@ import mechkit
 import mechinterfabric
 from mechinterfabric.abc import *
 
-deviators = mechinterfabric.deviators.deviators
+con = mechkit.notation.ConverterSymbolic()
+
+deviators = mechinterfabric.deviators.deviators[0:3]
 for dev in deviators:
     if dev.__name__ != "triclinic":
         deviator = sp.Matrix(dev())
         eigenvalues = deviator.eigenvals()
         eigenvectors = deviator.eigenvects()
+
         print("\n###########")
         print(dev.__name__)
         print(eigenvalues)
-        print(eigenvectors)
+        for ev in eigenvectors:
+            nicer = np.array([row for row in ev[2][0]])
+            print(f"Multiplicity={ev[1]}", nicer)
+            print(con.to_tensor(nicer))
+        # print(eigenvectors)
