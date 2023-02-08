9824a96ec5487e5ea1b464fa61674a16d218d487
JulianKarlBauer <juliankarlbauer@gmx.de>
2023-02-09 00:00:05 +0100

    Try to inspect rotated eigenvectors aswell

--- s005_eigenvectors_deviators.py
+++ s005_eigenvectors_deviators.py
@@ -4,19 +4,56 @@ import numpy as np
 import mechkit
 import mechinterfabric
 from mechinterfabric.abc import *
+from mechinterfabric import symbolic
 
 con = mechkit.notation.ConverterSymbolic()
 
-deviators = mechinterfabric.deviators.deviators
+deviators = mechinterfabric.deviators.deviators[0:4]
+
 for dev in deviators:
     if dev.__name__ != "triclinic":
         deviator = sp.Matrix(dev())
-        eigenvalues = deviator.eigenvals()
-        eigenvectors = deviator.eigenvects()
 
+        eigenvalues = deviator.eigenvals()
         print("\n###########")
         print(dev.__name__)
         print(eigenvalues)
+
+        eigenvectors = deviator.eigenvects()
+        for ev in eigenvectors:
+            nicer = np.array([row for row in ev[2][0]])
+            print(f"Multiplicity={ev[1]}")
+            print(con.to_tensor(nicer))
+        # print(eigenvectors)
+
+
+######################################################
+# Rotate and inspect again
+
+
+alpha = sp.symbols("alpha", real=True)
+
+Q = np.array(
+    (
+        (sp.cos(alpha), -sp.sin(alpha), z),
+        (sp.sin(alpha), sp.cos(alpha), z),
+        (z, z, one),
+    ),
+)
+
+
+print("#############")
+
+for dev in deviators:
+    if dev.__name__ != "triclinic":
+        deviator = sp.Matrix(symbolic.actively_rotate_mandel(dev(), Q))
+
+        eigenvalues = deviator.eigenvals()
+        print("\n###########")
+        print(dev.__name__)
+        print(eigenvalues)
+
+        eigenvectors = deviator.eigenvects()
         for ev in eigenvectors:
             nicer = np.array([row for row in ev[2][0]])
             print(f"Multiplicity={ev[1]}")
