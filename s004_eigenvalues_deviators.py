cf62e30b16d5f456d1d3a69db928564c1f8d9813
JulianKarlBauer <juliankarlbauer@gmx.de>
2023-02-08 23:53:03 +0100

    Move rotation to symbolic module

--- s004_eigenvalues_deviators.py
+++ s004_eigenvalues_deviators.py
@@ -1,9 +1,9 @@
 from pprint import pprint
 import sympy as sp
 import numpy as np
-import mechkit
 import mechinterfabric
 from mechinterfabric.abc import *
+from mechinterfabric import symbolic
 
 
 def myprint(dev, eigenvalues):
@@ -34,39 +34,12 @@ Q = np.array(
     ),
 )
 
-con = mechkit.notation.ConverterSymbolic()
-
-
-def mandel(tensor):
-    return con.to_mandel6(to_numpy(tensor))
-
-
-def tensorr(mandel6):
-    return con.to_tensor(to_numpy(mandel6))
-
-
-def to_numpy(inp):
-    return np.array(inp.tolist(), dtype=sp.Symbol)
-
-
-def actively_rotate_tensor(tensor, Q):
-    order = len(tensor.shape)
-    for i in range(order):
-        tensor = np.tensordot(Q, tensor, axes=[[1], [i]])
-    return tensor
-
-
-def actively_rotate_mandel(inp, Q):
-    tensor = tensorr(inp)
-    rotated = actively_rotate_tensor(tensor=tensor, Q=Q)
-    return mandel(rotated)
-
 
 print("#############")
 
 for dev in deviators:
     if dev.__name__ != "triclinic":
-        deviator = sp.Matrix(actively_rotate_mandel(dev(), Q))
+        deviator = sp.Matrix(symbolic.actively_rotate_mandel(dev(), Q))
         eigenvalues = deviator.eigenvals()
         # eigenvalues = {
         #     sp.trigsimp(key): val for key, val in deviator.eigenvals().items()
