b835520e2764167059ae8e3062435bc9e5eef048
JulianKarlBauer <juliankarlbauer@gmx.de>
2023-03-02 15:35:34 +0100

    Copy s006

---
+++ s007_tigonal.py
@@ -0,0 +1,44 @@
+from pprint import pprint
+
+import mechkit
+import numpy as np
+import sympy as sp
+
+import mechinterfabric
+from mechinterfabric import symbolic
+from mechinterfabric.abc import *
+
+con = mechkit.notation.ConverterSymbolic()
+
+deviator = mechinterfabric.deviators.deviators[2]
+print(deviator.__name__)
+
+
+def inspect(deviator):
+    matrix = sp.Matrix(deviator)
+
+    eigenvalues = matrix.eigenvals()
+    print("\n###########")
+    print(eigenvalues)
+
+    eigenvectors = matrix.eigenvects()
+    for ev in eigenvectors:
+        print(f"{ev[0]} with multiplicity={ev[1]}")
+        for eigenspace in ev[2]:
+            nicer = np.array([row for row in eigenspace])
+            print(con.to_tensor(nicer))
+
+            eigenvectors_eigenspace = sp.Matrix(
+                con.to_tensor(np.array(eigenspace.tolist())[:, 0])
+            ).eigenvects()
+            for ev_eigenspace in eigenvectors_eigenspace:
+                print(f"\t{ev_eigenspace[0]} with multiplicity={ev_eigenspace[1]}")
+                for eigenspace_eigenspace in ev_eigenspace[2]:
+                    nicer_eigenspace = np.array([row for row in eigenspace_eigenspace])
+                    print(f"\t{nicer_eigenspace}")
+
+            # raise Exception()
+    # print(eigenvectors)
+
+
+inspect(deviator())
