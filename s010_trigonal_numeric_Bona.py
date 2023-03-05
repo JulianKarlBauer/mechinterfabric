88d5f885efef35b6173210b96f474dbf75cb4a66
JulianKarlBauer <juliankarlbauer@gmx.de>
2023-03-05 12:13:59 +0100

    Do one step

--- s010_trigonal_numeric_Bona.py
+++ s010_trigonal_numeric_Bona.py
@@ -9,7 +9,7 @@ import mechinterfabric
 from mechinterfabric import symbolic
 from mechinterfabric.abc import *
 
-np.set_printoptions(linewidth=100000, precision=4)
+np.set_printoptions(linewidth=100000, precision=5)
 
 
 con = mechkit.notation.Converter()
@@ -35,6 +35,7 @@ print(f"deviator=\n{deviator}")
 rotation = mechinterfabric.utils.get_rotation_by_vector(
     vector=51 * np.array([1, 1.5, 1.3]), degrees=True
 )
+# rotation = mechinterfabric.utils.get_random_rotation()
 fot4_rotated = mechinterfabric.utils.rotate_to_mandel(fot4, Q=rotation)
 deviator_rotated = con.to_mandel6(mechkit.operators.dev(con.to_tensor(fot4_rotated)))
 # print(f"deviator_rotated=\n{deviator_rotated}")
@@ -71,13 +72,18 @@ for index in indices_eigenvectors_double_eigenvalues:
 
     tensor = con.to_tensor(evec)
 
-    B = tensor[1:, 1:]
-    b = tensor[0, 1:]
+    # B = tensor[1:, 1:]
+    # b = tensor[0, 1:]
 
-    a3 = B[0, 0]
-    a6 = B[0, 1]
-    a4 = b[0]
-    a5 = b[-1]
+    # a3 = B[0, 0]
+    # a6 = B[0, 1]
+    # a4 = b[0]
+    # a5 = b[-1]
+
+    a3 = tensor[2, 2]
+    a6 = tensor[1, 2]
+    a4 = tensor[0, 1]
+    a5 = tensor[0, 2]
 
     eta = np.arccos(a3 / (np.sqrt(a3**2 + a6**2)))
     theta = np.arccos(a4 / (np.sqrt(a4**2 + a5**2)))
@@ -87,9 +93,21 @@ for index in indices_eigenvectors_double_eigenvalues:
     rotation = mechinterfabric.utils.get_rotation_by_vector(
         vector=angle * np.array([1, 0, 0]), degrees=False
     )
+
+    # tensor_rotated = np.einsum("ij, kl, jl->ik", rotation, rotation, tensor)
     deviator_reconstructed_rotated = mechinterfabric.utils.rotate_to_mandel(
         spectral_decomposition.deviator, Q=rotation
     )
+
+    # index_pos_d9 = np.s_[1, 5]
+    # if deviator_reconstructed_rotated[index_pos_d9] <= 0:
+    #     print("######################## \tinverted\t########################")
+    #     rotation_changing_sign = np.array(
+    #         [[1, 0, 0], [0, -1, 0], [0, 0, -1]], dtype=np.float64
+    #     )
+    #     deviator_reconstructed_rotated = mechinterfabric.utils.rotate_to_mandel(
+    #         spectral_decomposition.deviator, Q=rotation_changing_sign
+    #     )
 
     ########################
 
@@ -97,12 +115,16 @@ for index in indices_eigenvectors_double_eigenvalues:
 
     print(f"\nDouble Eigenvalue {eval} has eigen tensor")
     print(tensor)
+    # print(f"tensor_rotated = {tensor_rotated}")
 
-    print(f"B={B}")
-    print(f"b={b}")
+    # print(f"B={B}")
+    # print(f"b={b}")
+    print(f"a3={a3}\ta6={a6}\ta4={a4}\ta5={a5}")
 
     print(f"eta={np.rad2deg(eta)}°")
     print(f"theta={np.rad2deg(theta)}°")
     print(f"angle={np.rad2deg(angle)}°")
 
-    print(f"deviator_reconstructed_rotated=\n{deviator_reconstructed_rotated}")
+    difference = deviator_reconstructed_rotated - deviator
+
+    print(f"difference=\n{np.round(difference, 5)}")
