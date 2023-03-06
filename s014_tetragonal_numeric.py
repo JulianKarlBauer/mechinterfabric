bc9404991c43132c8a8d3c98b4f4647010201ac1
JulianKarlBauer <juliankarlbauer@gmx.de>
2023-03-06 21:25:20 +0100

    Play with tetragonal case and identify strange 45 degree things

--- s014_tetragonal_numeric.py
+++ s014_tetragonal_numeric.py
@@ -25,12 +25,12 @@ def lambdified_parametrization():
     )
 
 
-fot4 = lambdified_parametrization()(**{"alpha1": 0, "d1": 0.025, "d3": 0.015})
+fot4 = lambdified_parametrization()(**{"alpha1": 0, "d1": 0.025, "d3": 0.013})
 assert np.min(np.linalg.eigh(fot4)[0]) >= 0, "has to be positive semi definite"
 
 # fot4 = con.to_mandel6(mechkit.operators.dev(con.to_tensor(fot4)))
 
-angles = [270 * i for i in range(5)]
+angles = np.arange(0, 90 + 45, 45)
 candidates = []
 for angle in angles:
     rotation = mechinterfabric.utils.get_rotation_by_vector(
@@ -41,12 +41,13 @@ for angle in angles:
 
     indices = np.s_[[0, 0, 0, 1, 1, 2, 2], [3, 4, 5, 3, 4, 3, 4]]
     residuum = np.linalg.norm(rotated[indices])
-    # if residuum <= 1e-5:
-    print(f"angle={angle}\n {np.round(rotated, 6)}")
-    candidates.append(rotated)
+    if residuum <= 1e-5:
+        print(f"angle={angle} with fot4=\n {np.round(rotated, 6)}")
+        candidates.append(rotated)
 
+print("#######")
 
-for candidate in candidates:
+for angle, candidate in zip(angles, candidates):
     assert np.linalg.det(candidate) >= 0
     assert np.min(np.linalg.eigh(candidate)[0]) >= 0
     fot2 = np.tensordot(con.to_tensor(fot4), np.eye(3))
@@ -54,8 +55,25 @@ for candidate in candidates:
     sym = mechkit.operators.Sym()
     assert np.allclose(sym(candidate), candidate)
 
-    assert np.allclose(candidate, fot4)
+    # assert np.allclose(candidate, fot4)
+    deviator = con.to_mandel6(mechkit.operators.dev(con.to_tensor(candidate)))
 
+    print(f"angle={angle} with deviator=\n {np.round(deviator, 6)}")
+
+    assert np.isclose(deviator[0, 2] + deviator[1, 2], -deviator[2, 2])
+
+    # Prepare mathematica
+    # tmp = vofotensors.fabric_tensors.N4s_parametric["tetragonal"]["alpha1_d1_d3"]
+    # str(tmp).replace('[', '{').replace(']', '}')
+
+    # tmp = vofotensors.fabric_tensors.N4s_parametric["trigonal"]['alpha1_d3_d9']
+    # str(tmp).replace('[', '{').replace(']', '}')
+
+    d1 = deviator[0, 2]
+    d3 = deviator[1, 2]
+    assert (-1 / 15 <= d1) and (d1 <= 2 / 45)
+    assert -1 / 15 <= d1
+    assert d3 <= 1 / 210 * (14 - 105 * d1)
 
 print("###################")
 
