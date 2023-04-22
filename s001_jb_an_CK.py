ba359cfe941c6d1cab7de3b13b5631196193197d
JulianKarlBauer <juliankarlbauer@gmx.de>
2023-04-22 18:07:45 +0200

    Plot again

--- s001_jb_an_CK.py
+++ s001_jb_an_CK.py
@@ -13,6 +13,8 @@ from symbolic.numbers import one
 from symbolic.numbers import z
 from vofotensors.abc import d1
 
+import mechinterfabric.visualization_matplotlib
+
 N4 = vofotensors.fabric_tensors.N4s_parametric["cubic"]["d1"]
 
 pprint(N4)
@@ -77,68 +79,68 @@ def matrix_are_equal(A, B):
 
 assert matrix_are_equal(N2, N2_rotated)
 
-# #########################
-# # Plot
-# converter = mechkit.notation.Converter()
+#########################
+# Plot
+converter = mechkit.notation.Converter()
 
 
-# def sym_to_num_tensor(N4):
-#     return converter.to_tensor(np.array(N4, dtype=np.float64))
+def sym_to_num_tensor(N4):
+    return converter.to_tensor(np.array(N4, dtype=np.float64))
 
 
-# low_num = sym_to_num_tensor(low)
-# high_num = sym_to_num_tensor(high)
+low_num = sym_to_num_tensor(low)
+high_num = sym_to_num_tensor(high)
 
-# fig = plt.figure()
-# ax = fig.add_subplot(111, projection="3d")
+fig = plt.figure()
+ax = fig.add_subplot(111, projection="3d")
 
-# plot_func_key = "cos_fodf"
+plot_func_key = "cos_fodf"
 
-# mechinterfabricdev.visualization.plot_stepwise_interpolation_N4_along_x(
-#     ax=ax,
-#     N1=low_num,
-#     N2=high_num,
-#     nbr_points=7,
-#     scale=5,
-#     method=lambda N4s, weights: mechinterfabricdev.interpolation.interpolate_N4_decomp_unique_rotation_extended_return_values(
-#         N4s=N4s,
-#         weights=weights,
-#         func_interpolation_rotation=mechinterfabricdev.rotation.average_Manton2004,
-#     )[
-#         0
-#     ],
-#     origin_y=0,
-#     origin_z=0,
-#     plot_func_key=plot_func_key,
-# )
+mechinterfabric.visualization_matplotlib.plot_stepwise_interpolation_N4_along_x(
+    ax=ax,
+    N1=low_num,
+    N2=high_num,
+    nbr_points=7,
+    scale=5,
+    method=lambda N4s, weights: mechinterfabric.interpolation.interpolate_N4_decomp_unique_rotation_extended_return_values(
+        N4s=N4s,
+        weights=weights,
+        func_interpolation_rotation=mechinterfabric.rotation.average_Manton2004,
+    )[
+        0
+    ],
+    origin_y=0,
+    origin_z=0,
+    plot_func_key=plot_func_key,
+)
 
 
-# upper = 3
-# lower = 0
-# offset = 1
-# limits = [
-#     (lower - offset, upper + offset),
-#     (-0.5 - offset, 0.5 + offset),
-#     (-0.5 - offset, 0.5 + offset),
-# ]
-# ax.set_xlim(limits[0])
-# ax.set_ylim(limits[1])
-# ax.set_zlim(limits[2])
+upper = 3
+lower = 0
+offset = 1
+limits = [
+    (lower - offset, upper + offset),
+    (-0.5 - offset, 0.5 + offset),
+    (-0.5 - offset, 0.5 + offset),
+]
+ax.set_xlim(limits[0])
+ax.set_ylim(limits[1])
+ax.set_zlim(limits[2])
 
-# # Axes labels
-# ax.set_xlabel("x")
-# ax.set_ylabel("y")
-# ax.set_zlabel("z")
+# Axes labels
+ax.set_xlabel("x")
+ax.set_ylabel("y")
+ax.set_zlabel("z")
 
-# # Homogeneous axes
-# bbox_min = np.min(limits)
-# bbox_max = np.max(limits)
-# ax.auto_scale_xyz([bbox_min, bbox_max], [bbox_min, bbox_max], [bbox_min, bbox_max])
+# Homogeneous axes
+bbox_min = np.min(limits)
+bbox_max = np.max(limits)
+ax.auto_scale_xyz([bbox_min, bbox_max], [bbox_min, bbox_max], [bbox_min, bbox_max])
 
-# key = "cubic"
+key = "cubic"
 
-# ax.set_title(key)
-# directory = os.path.join("output", "s040")
-# os.makedirs(directory, exist_ok=True)
-# path_picture = os.path.join(directory, key + ".png")
-# plt.savefig(path_picture)
+ax.set_title(key)
+directory = os.path.join("output", "s001")
+os.makedirs(directory, exist_ok=True)
+path_picture = os.path.join(directory, key + ".png")
+plt.savefig(path_picture)
