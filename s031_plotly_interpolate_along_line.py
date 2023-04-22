c88aebeb2060a08137dccee88efe69dd095916b9
JulianKarlBauer <juliankarlbauer@gmx.de>
2023-04-22 19:28:41 +0200

    Plot random triclinic in a row

--- s031_plotly_interpolate_along_line.py
+++ s031_plotly_interpolate_along_line.py
@@ -26,9 +26,9 @@ fig.update_layout(scene_aspectmode="data")
 
 fig.update_layout(
     scene=dict(
-        xaxis=dict(showticklabels=False),
-        yaxis=dict(showticklabels=False),
-        zaxis=dict(showticklabels=False),
+        xaxis=dict(showticklabels=False, visible=False),
+        yaxis=dict(showticklabels=False, visible=False),
+        zaxis=dict(showticklabels=False, visible=False),
     )
 )
 
@@ -86,14 +86,23 @@ second = lambdified_parametrization_triclinic()(
 #     N4s=N4s, weights=np.array([0.5, 0.5])
 # )
 
+for seed in [5, 10]:
+    np.random.seed(5)
+    first = mechkit.fabric_tensors.first_kind_discrete(
+        orientations=np.random.rand(8, 3), order=4
+    )
+    second = mechkit.fabric_tensors.first_kind_discrete(
+        orientations=np.random.rand(5, 3), order=4
+    )
 
-visualization_plotly.plot_stepwise_interpolation_N4_along_x(
-    fig=fig,
-    N1=first,
-    N2=second,
-    nbr_points=5,
-    scale=2,
-    method=None,
-)
+    visualization_plotly.plot_stepwise_interpolation_N4_along_x(
+        fig=fig,
+        N1=first,
+        N2=second,
+        nbr_points=5,
+        scale=2.5,
+        method=None,
+        nbr_vectors=300,
+    )
 
-fig.show()
+    fig.show()
