62eeb899c6e90799c7526b02140c93340cbac83d
JulianKarlBauer <juliankarlbauer@gmx.de>
2023-04-22 13:57:08 +0200

    Plot color coded FODF

--- s030_plotly_3D.py
+++ s030_plotly_3D.py
@@ -2,9 +2,12 @@ import matplotlib.pyplot as plt
 import mechkit
 import numpy as np
 import plotly.graph_objects as go
+import sympy as sp
+import vofotensors
 from plotly.subplots import make_subplots
 
 import mechinterfabric
+from mechinterfabric.abc import *
 
 # Equation of ring cyclide
 # see https://en.wikipedia.org/wiki/Dupin_cyclide
@@ -26,18 +29,24 @@ fig = make_subplots(
         "Subplot title",
     ],
 )
+fig.layout.scene.update(
+    aspectmode="data",
+    # aspectmode="cube",
+    # aspectratio=dict(x=1, y=1, z=1),
+)
+
+####################
 
 cmap = plt.get_cmap("tab10")
 # colorscale = [[0, "rgb" + str(cmap(1)[0:3])], [1, "rgb" + str(cmap(2)[0:3])]]
 colorscale = [
     [0, "rgb(1.0, 0.5, 0.05)"],
-    [1, "rgb(0.17, 0.63, 0.17)"],
+    [1, "rgb(0.2, 0.7, 0.2)"],
 ]
 
 
 offset = 1
 options = dict(
-    surfacecolor=np.ones_like(x),
     showscale=False,
     colorscale=colorscale,
 )
@@ -82,36 +91,53 @@ def get_data(
     return xyz, scalars
 
 
-N4 = mechkit.fabric_tensors.Basic().N4["iso"]
-xyz, scalars = get_data(N4=N4)
+def add_N4_plotly(fig, N4, origin=[0, 0, 0], nbr_points=100, options={}):
+    xyz, scalars = get_data(N4=N4, origin=origin, nbr_points=nbr_points)
+    # raise Exception("")
+    fig.add_trace(
+        go.Surface(
+            x=xyz[0],
+            y=xyz[1],
+            z=xyz[2],
+            surfacecolor=np.sign(scalars),
+            # surfacecolor=np.random.rand(*scalars.shape),
+            **options
+        ),
+        1,
+        1,
+    )
 
-fig.add_trace(
-    go.Surface(
-        x=xyz[0],
-        y=xyz[1],
-        z=xyz[2],
-        **options,
-    ),
-    1,
-    1,
+
+def lambdified_parametrization_triclinic():
+    return sp.lambdify(
+        [la1, la2, d1, d2, d3, d4, d5, d6, d7, d8, d9],
+        vofotensors.fabric_tensors.N4s_parametric["triclinic"][
+            "la1_la2_d1_d2_d3_d4_d5_d6_d7_d8_d9"
+        ],
+    )
+
+
+kwargs = {
+    "la1": 1 / 2,
+    "la2": 1 / 4,
+    "d1": 0.05,
+    "d2": 0.033,
+    "d3": 0.011,
+}
+
+
+N4 = lambdified_parametrization_triclinic()(
+    d4=0, d5=0, d6=0, d7=0, d8=0, d9=0, **kwargs
 )
 
-fig.add_trace(
-    go.Surface(
-        x=xyz[0] + offset,
-        y=xyz[1],
-        z=xyz[2],
-        **options,
-    ),
-    1,
-    1,
+add_N4_plotly(
+    fig=fig,
+    N4=N4,
+    origin=[0, 0, 0],
+    options=options,
 )
 
-fig.layout.scene.update(
-    aspectmode="data",
-    # aspectmode="cube",
-    # aspectratio=dict(x=1, y=1, z=1),
-)
+
 # fig.update_layout(title_text="Title")
 # fig.update_layout(scene_aspectmode="cube")
 # fig.update_layout(scene_aspectmode="manual", scene_aspectratio=dict(x=1, y=1, z=2))
