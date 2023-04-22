eebcc3190bf2ae6aa4d2b017f9199b11fe2b9c09
JulianKarlBauer <juliankarlbauer@gmx.de>
2023-04-22 14:09:51 +0200

    Color coding homogeneous not yet working

--- s030_plotly_3D.py
+++ s030_plotly_3D.py
@@ -9,17 +9,8 @@ from plotly.subplots import make_subplots
 import mechinterfabric
 from mechinterfabric.abc import *
 
-# Equation of ring cyclide
-# see https://en.wikipedia.org/wiki/Dupin_cyclide
-
-a, b, d = 1.32, 1.0, 0.8
-c = a**2 - b**2
-u, v = np.mgrid[0 : 2 * np.pi : 100j, 0 : 2 * np.pi : 100j]
-x = (d * (c - a * np.cos(u) * np.cos(v)) + b**2 * np.cos(u)) / (
-    a - c * np.cos(u) * np.cos(v)
-)
-y = b * np.sin(u) * (a - d * np.cos(v)) / (a - c * np.cos(u) * np.cos(v))
-z = b * np.sin(v) * (c * np.cos(u) - d) / (a - c * np.cos(u) * np.cos(v))
+############################
+# Set figure
 
 fig = make_subplots(
     rows=1,
@@ -29,16 +20,13 @@ fig = make_subplots(
         "Subplot title",
     ],
 )
-fig.layout.scene.update(
-    aspectmode="data",
-    # aspectmode="cube",
-    # aspectratio=dict(x=1, y=1, z=1),
-)
+fig.update_layout(scene_aspectmode="data")
 
-####################
+
+############################
+# Define colors
 
 cmap = plt.get_cmap("tab10")
-# colorscale = [[0, "rgb" + str(cmap(1)[0:3])], [1, "rgb" + str(cmap(2)[0:3])]]
 colorscale = [
     [0, "rgb(1.0, 0.5, 0.05)"],
     [1, "rgb(0.2, 0.7, 0.2)"],
@@ -52,19 +40,15 @@ options = dict(
 )
 
 
-# fig.add_trace(
-#     go.Surface(
-#         x=x,
-#         y=y,
-#         z=z,
-#         **options,
-#     ),
-#     1,
-#     1,
-# )
-
 ############################
 # N4
+
+
+def limit_scaling(scalars, limit_scalar):
+    maximum_scalar = np.max(scalars)
+    if np.max(scalars) > limit_scalar:
+        scalars = scalars * (limit_scalar / maximum_scalar)
+    return scalars
 
 
 def get_data(
@@ -80,32 +64,29 @@ def get_data(
     )
 
     scalars = distribution.calc_scalars(vectors)
+    scalars_limited = limit_scaling(scalars, limit_scalar=0.55)
 
-    maximum_scalar = np.max(scalars)
-    limit_scalar = 0.55
-    if np.max(scalars) > limit_scalar:
-        scalars = scalars * (limit_scalar / maximum_scalar)
+    xyz = scalars_limited * vectors + np.array(origin)[:, np.newaxis, np.newaxis]
 
-    xyz = scalars * vectors + np.array(origin)[:, np.newaxis, np.newaxis]
-
-    return xyz, scalars
+    return xyz, scalars_limited
 
 
 def add_N4_plotly(fig, N4, origin=[0, 0, 0], nbr_points=100, options={}):
+
     xyz, scalars = get_data(N4=N4, origin=origin, nbr_points=nbr_points)
-    # raise Exception("")
-    fig.add_trace(
-        go.Surface(
-            x=xyz[0],
-            y=xyz[1],
-            z=xyz[2],
-            surfacecolor=np.sign(scalars),
-            # surfacecolor=np.random.rand(*scalars.shape),
-            **options
-        ),
-        1,
-        1,
+
+    surface = go.Surface(
+        x=xyz[0],
+        y=xyz[1],
+        z=xyz[2],
+        surfacecolor=np.sign(scalars),
+        **options,
     )
+
+    fig.add_trace(surface)
+
+
+#################################################
 
 
 def lambdified_parametrization_triclinic():
@@ -117,31 +98,50 @@ def lambdified_parametrization_triclinic():
     )
 
 
-kwargs = {
-    "la1": 1 / 2,
-    "la2": 1 / 4,
-    "d1": 0.05,
-    "d2": 0.033,
-    "d3": 0.011,
-}
-
-
-N4 = lambdified_parametrization_triclinic()(
-    d4=0, d5=0, d6=0, d7=0, d8=0, d9=0, **kwargs
-)
-
 add_N4_plotly(
     fig=fig,
-    N4=N4,
+    N4=lambdified_parametrization_triclinic()(
+        la1=1 / 2,
+        la2=1 / 4,
+        d1=0.05,
+        d2=0.033,
+        d3=0.011,
+        d4=0,
+        d5=0,
+        d6=0,
+        d7=0,
+        d8=0,
+        d9=0,
+    ),
     origin=[0, 0, 0],
     options=options,
 )
 
+add_N4_plotly(
+    fig=fig,
+    N4=lambdified_parametrization_triclinic()(
+        la1=1 / 2,
+        la2=1 / 3,
+        d1=0.03,
+        d2=0.023,
+        d3=-0.01,
+        d4=0,
+        d5=0,
+        d6=0,
+        d7=0,
+        d8=0,
+        d9=0,
+    ),
+    origin=[-0.5, 0, 0],
+    options=options,
+)
 
-# fig.update_layout(title_text="Title")
-# fig.update_layout(scene_aspectmode="cube")
-# fig.update_layout(scene_aspectmode="manual", scene_aspectratio=dict(x=1, y=1, z=2))
-# fig.update_layout(scene_aspectmode="data")
+add_N4_plotly(
+    fig=fig,
+    N4=mechkit.fabric_tensors.Basic().N4["iso"],
+    origin=[-1, 0, 0],
+    options=options,
+)
 
 
 fig.show()
