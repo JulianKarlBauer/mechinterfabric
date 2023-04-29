b0f42221abc80346a0b19aa583b8ad82d132abb8
JulianKarlBauer <juliankarlbauer@gmx.de>
2023-04-29 13:56:57 +0200

    Combine views into one: Actually only average tensor components wihtout rotating anything

--- s034_plotly_interpolate_transv_fields.py
+++ s034_plotly_interpolate_transv_fields.py
@@ -27,16 +27,16 @@ os.makedirs(directory, exist_ok=True)
 converter = mechkit.notation.ExplicitConverter()
 con = mechkit.notation.Converter()
 
-
 #########################################################
 
 almost_zero = 1e-5
 
 index_low, index_medium, index_high = 1, 3, 5
 
+
 df = pd.DataFrame(
     [
-        # ["planar_iso", -2 / 6, 3 / 280, index_low, index_high],
+        ["planar_iso", -2 / 6, 3 / 280, index_low, index_high],
         ["iso", almost_zero, 0, index_medium, index_medium],
         ["ud", 4 / 6, 1 / 35, index_high, index_low],
         ["iso2_max", almost_zero, 1 / 60, index_high, index_high],
@@ -52,19 +52,6 @@ N4_func = sp.lambdify([alpha1, rho1], parameterization)
 
 df["N4"] = df.apply(lambda row: N4_func(alpha1=row["alpha1"], rho1=row["rho1"]), axis=1)
 
-# Q = Rotation.from_rotvec(np.pi / 2 * np.array([0, 1, 0])).as_matrix()
-# df = df.set_index("label")
-# df.at["planar_iso", "N4"] = con.to_mandel6(
-#     np.einsum(
-#         "io,jm,kn,lp, omnp->ijkl",
-#         Q,
-#         Q,
-#         Q,
-#         Q,
-#         con.to_tensor(df.loc["planar_iso"]["N4"]),
-#     )
-# )
-# df.reset_index()
 
 N4s = converter.convert(
     source="mandel6",
@@ -85,7 +72,7 @@ indices_points = list(itertools.product(indices, repeat=2))
 indices_points = [
     index
     for index in indices_points
-    if ((index not in df_index) and (index[0] >= index[1]))
+    if ((index not in df_index))  # and (index[0] >= index[1]))
 ]
 
 new = pd.DataFrame(indices_points, columns=["index_x", "index_y"])
@@ -146,8 +133,15 @@ def do_not_rotate(matrices, weights):
     return np.eye(3)
 
 
+def average_in_eigensystem(N4s, weights, func_interpolation_rotation=None):
+    # Average components in eigensystems
+    N4_av = np.einsum("m, mijkl->ijkl", weights, N4s)
+    return [N4_av, None]
+
+
 for interpolation_method in [
-    mechinterfabric.interpolation.interpolate_N4_decomp_unique_rotation_extended_return_values,
+    # mechinterfabric.interpolation.interpolate_N4_decomp_unique_rotation_extended_return_values,
+    average_in_eigensystem,
 ]:
 
     new["N4"] = new.apply(
@@ -243,10 +237,4 @@ for interpolation_method in [
 
         fig.update_layout(scene=dict(annotations=plotter.annotation_bucket))
 
-
-# # Inspect unknown N4s
-# tmp = new.set_index(["index_x", "index_y"])
-# N4_new = con.to_mandel6(tmp.loc[(1, 2)]["N4"])
-# print(np.linalg.eig(N4_new))
-# print(np.einsum("i,ji->j", [1, 1, 1, 0, 0, 0], N4_new))
-# print(sum(np.einsum("i,ji->j", [1, 1, 1, 0, 0, 0], N4_new)))
+        fig.show()
