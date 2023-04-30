a95e4ebf0a28384b1918e8501648206cd393d56f
JulianKarlBauer <juliankarlbauer@gmx.de>
2023-04-30 14:12:50 +0200

    Fix name output dir

--- s034_plotly_interpolate_transv_fields.py
+++ s034_plotly_interpolate_transv_fields.py
@@ -20,7 +20,7 @@ from mechinterfabric.abc import *
 np.random.seed(seed=100)
 np.set_printoptions(linewidth=100000)
 
-directory = os.path.join("output", "s032")
+directory = os.path.join("output", "s034")
 os.makedirs(directory, exist_ok=True)
 
 
